2ebcd26ba2931e49f200a08484b55854471275c8
Aitor Zaldua <aitor.zaldua@gmail.com>
2020-11-15 12:38:15 +0100

    november 15 commit 2

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HolaMundo = () => {
+    const Hello = "Hola Mundo!"
+    return(
+        <div className="HolaMundo">
+            <h1>{Hello}</h1>
+            <h2>Curso Esencial de React</h2>
+            <img src="/mnt/c/2_Javascript/react/holamundo/public/mickey.png" alr="React" />
+        </div>
+
+    );
+
+};
